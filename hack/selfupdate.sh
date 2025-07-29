34feec96fa89308d38dbf6c77ddc219c70e31813
Adam Cmiel <acmiel@redhat.com>
2025-07-29 16:42:27 +0200

    Copy files directly instead of using cruft
    Applying the template using 'cruft create' on the same repo where the
    the template is defined brings too many problems. It was a useful
    procedure to get familiar with cruft, but let's move away from it.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- hack/selfupdate.sh
+++ hack/selfupdate.sh
@@ -2,30 +2,19 @@
 set -o errexit -o nounset -o pipefail
 
 files_in_danger=$(
-    # print the files/directories at the first level of {{cookiecutter.repo_root}}
-    # (just the names, i.e. print as if they were in the root directory)
-    find '{{cookiecutter.repo_root}}' -mindepth 1 -maxdepth 1 -printf '%f\0' |
-        # check status of files/directories generated from template
-        xargs -0 git status --porcelain --
+    find '{{cookiecutter.repo_root}}' -type f | cut -d / -f 2- | xargs git status -s
 )
 
 if [[ -n "$files_in_danger" ]]; then
-    echo "Refusing to proceed, could overwrite/delete the following files:"
+    echo "Refusing to proceed, could overwrite the following files:"
     # shellcheck disable=SC2001
-    sed 's/^/  /' <<< "${files_in_danger}"
+    sed 's/^/  /' <<< "$files_in_danger"
     exit 1
 fi >&2
 
-# re-generate from template
-uv run cruft create . --no-input --overwrite-if-exists
-
-# restore deleted files if they were not from template
-git diff --diff-filter=D --name-only | while read -r deleted_file; do
-    if ! git show HEAD:"${deleted_file}" | grep -q "^#.*<TEMPLATED FILE!>"; then
-        printf '%s\0' "$deleted_file"
-    fi
-done | xargs -0 git checkout
-
-# stage templated files
-find '{{cookiecutter.repo_root}}' -mindepth 1 -maxdepth 1 -printf '%f\0' |
-    xargs -0 git add
+find '{{cookiecutter.repo_root}}' -type f | while read -r actual_path; do
+    copy_to=${actual_path#*/}
+    mkdir -p "$(dirname "$copy_to")"
+    cp --remove-destination "$actual_path" "$copy_to"
+    git add "$copy_to"
+done
