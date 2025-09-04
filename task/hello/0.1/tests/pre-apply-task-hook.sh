99fb764d0693eb83959ee4884bd8afdfb58f8c26
Filip Mudry <fmudry@redhat.com>
2025-09-04 11:02:32 +0200

    add integration test for the `hello` task
    I've also added a dummy `pre-apply-task-hook.sh`, so that onboarding developers know about this feature.
    
    Signed-off-by: Filip Mudry <fmudry@redhat.com>

---
+++ task/hello/0.1/tests/pre-apply-task-hook.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is called before applying the task to set up required resources
+# This is just a dummy version, for demonstration purposes
+
+TASK_COPY="$1"
+TEST_NS="$2"
+
+echo "TASK_COPY: $TASK_COPY"
+echo "TEST_NS: $TEST_NS"
