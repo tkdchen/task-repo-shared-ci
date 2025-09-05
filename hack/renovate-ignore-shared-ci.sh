bb66a4a69f4353c13c689252adb10868a287014c
Adam Cmiel <acmiel@redhat.com>
2025-09-05 11:18:35 +0200

    renovate-ignore-shared-ci: handle all config paths
    Don't require the repo to have exactly renovate.json, support all the
    paths documented in https://docs.renovatebot.com/configuration-options/
    
    This does not mean we support JSON5 syntax, we still support only
    regular JSON. But the file extension doesn't have much impact on that
    (Renovate handles JSON5 in .json files), so let's just look through all
    the paths and hope for the best.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- hack/renovate-ignore-shared-ci.sh
+++ hack/renovate-ignore-shared-ci.sh
@@ -6,16 +6,45 @@ set -o errexit -o nounset -o pipefail
 # Please consider sending a PR upstream instead of editing the file directly.
 # See the SHARED-CI.md document in this repo for more details.
 
+# Finds the Renovate configuration file (typically renovate.json) in the repo and updates
+# ignorePaths to include all the .github/ files that come from the Shared CI repository.
+#
+# If there is no Renovate configuration file, creates a new renovate.json.
+
+# https://docs.renovatebot.com/configuration-options/
+# We don't support json5, but let's ignore the file extension
+#   .json doesn't mean it's not JSON5, and .json5 doesn't mean it's not regular JSON
+#   Leave it to jq to fail if failure is unavoidable
+renovate_config_paths=(
+    renovate.json
+    renovate.json5
+    .github/renovate.json
+    .github/renovate.json5
+    .gitlab/renovate.json
+    .gitlab/renovate.json5
+    .renovaterc
+    .renovaterc.json
+    .renovaterc.json5
+)
+
+renovate_config_path=${renovate_config_paths[0]}
+for filepath in "${renovate_config_paths[@]}"; do
+    if [[ -e "$filepath" ]]; then
+        renovate_config_path=$filepath
+        break
+    fi
+done
+
 shared_ci_files=$(
     grep -R '^# <TEMPLATED FILE!>' .github/ --files-with-matches |
     jq --raw-input | jq --slurp --compact-output
 )
 
 new_renovate_json=$(
-    if [[ -s renovate.json ]]; then
-        cat renovate.json
+    if [[ -s "$renovate_config_path" ]]; then
+        cat "$renovate_config_path"
     else
-        # renovate.json empty or missing => use default config
+        # $renovate_config_path empty or missing => use default config
         # https://docs.renovatebot.com/config-overview/#onboarding-config
         jq -n '{
             "$schema": "https://docs.renovatebot.com/renovate-schema.json"
@@ -31,12 +60,12 @@ new_renovate_json=$(
 )
 
 if [[ -z "$new_renovate_json" ]]; then
-    echo "renovate.json is up to date" >&2
+    echo "$renovate_config_path is up to date" >&2
 else
-    if [[ -e renovate.json ]]; then
-        echo "updated renovate.json" >&2
+    if [[ -e "$renovate_config_path" ]]; then
+        echo "updated $renovate_config_path" >&2
     else
-        echo "created renovate.json" >&2
+        echo "created $renovate_config_path" >&2
     fi
-    printf '%s\n' "$new_renovate_json" > renovate.json
+    printf '%s\n' "$new_renovate_json" > "$renovate_config_path"
 fi
